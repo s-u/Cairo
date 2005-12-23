be6b4f47b01fea9920fc9730780cbd2f945b9b9c
Simon Urbanek <simon.urbanek@r-project.org>
2005-12-23 20:32:35 +0000

    Fix types in the backend API
    git-svn-id: svn://svn.rforge.net/Cairo/trunk@5 29998a20-39e5-47a2-8342-695c7f2dfac2

--- src/img-backend.c
+++ src/img-backend.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include "backend.h"
 
-static cairo_surface_t *image_backend_create_surface(struct Rcairo_backend* be, int width, int height)
+static cairo_surface_t *image_backend_create_surface(Rcairo_backend* be, int width, int height)
 {
   int stride = 4 * width;
-  unsigned char *buf;
   void *buf = calloc (stride * height, 1);
   
   return cairo_image_surface_create_for_data (buf,
@@ -12,7 +11,7 @@ static cairo_surface_t *image_backend_create_surface(struct Rcairo_backend* be, 
 					      width, height, stride);
 }
 
-static void image_backend_destroy_surface()
+static void image_backend_destroy_surface(Rcairo_backend* be)
 {
 }
 
