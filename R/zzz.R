2fcc80099ec000957a92afb842415551c1b1cfe6
Simon Urbanek <simon.urbanek@r-project.org>
2007-04-12 23:35:59 +0000

    Added namespace, remove putenv
    git-svn-id: svn://svn.rforge.net/Cairo/trunk@42 29998a20-39e5-47a2-8342-695c7f2dfac2

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,13 @@
 .First.lib <- function(libname, pkgname) {
-  if (.Platform$OS.type=="windows") {
-    lp<-gsub("/","\\\\",paste(libname,pkgname,"libs",sep="/"))
-    cp<-strsplit(Sys.getenv("PATH"),";")
-    if (! lp %in% cp) Sys.putenv(PATH=paste(lp,Sys.getenv("PATH"),sep=";"))
-  }
-  library.dynam("Cairo", pkgname, libname)
+    ## add our libs to the PATH
+    if (.Platform$OS.type=="windows") {
+        .setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
+        lp<-gsub("/","\\\\",paste(libname,pkgname,"libs",sep="/"))
+        cp<-strsplit(Sys.getenv("PATH"),";")
+        if (! lp %in% cp) .setenv(PATH=paste(lp,Sys.getenv("PATH"),sep=";"))
+    }
+    library.dynam("Cairo", pkgname, libname)
 }
+
+### in case we decide to keep the namespace ...
+.onLoad <- .First.lib
