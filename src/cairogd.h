ad534ee83fcc7f34fb852857806cbec802b782d2
Simon Urbanek <simon.urbanek@r-project.org>
2013-12-10 13:47:32 -0500

    bump the version

--- src/cairogd.h
+++ src/cairogd.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_GD_H
 #define _DEV_GD_H
 
-#define CAIROGD_VER 0x010503 /* Cairo v1.5-3 */
+#define CAIROGD_VER 0x010504 /* Cairo v1.5-4 */
 
 /* cairo R package config */
 #include "cconfig.h"
