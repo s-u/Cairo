163898082bb72a11c5789525b184c3e7ee5c9be1
Simon Urbanek <simon.urbanek@R-project.org>
2022-04-27 14:21:39 +1200

    bump version to 1.6-0

--- src/cairogd.h
+++ src/cairogd.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_GD_H
 #define _DEV_GD_H
 
-#define CAIROGD_VER 0x01050f /* Cairo v1.5-15 */
+#define CAIROGD_VER 0x010600 /* Cairo v1.6-0 */
 
 /* cairo R package config */
 #include "cconfig.h"
